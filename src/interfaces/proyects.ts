fa209d295e60fedb69adf7997fbe785f7b5e7fc4
unknown <andresjose.210299@gmail.com>
2024-07-18 21:00:22 -0400

    Projects section added

---
+++ src/interfaces/proyects.ts
@@ -0,0 +1,17 @@
+export interface IProyects {
+    title: string;
+    description: string;
+    link: string;
+    gitHub: string;
+    image:string;
+    tags: ITags[]
+  }
+
+  export interface ITags {
+    name: string;
+    class: string;
+    icon: HTMLBodyElement
+  }
+
+
+  
