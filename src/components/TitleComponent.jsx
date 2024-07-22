70f5ecf90a42dc36a6813ef3410ee3351f7273fe
Jose Martinez <andresjose.210299@gmail.com>
2024-07-21 23:10:07 -0400

    title animation

---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+const TitleComponent = () => {
+  const titles = ["I am Andrés   ", "I am Developer", "Andreveloper!  "];
+  const [index, setIndex] = useState(0);
+  const [animationActive, setAnimationActive] = useState(true);
+
+  useEffect(() => {
+
+    if (index < titles.length - 1) {
+      // Si no es el último título, mantener la animación
+      const interval = setInterval(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % titles.length);
+      }, 6000);
+
+      return () => clearInterval(interval);
+    } else {
+      // Si es el último título, detener la animación
+      setAnimationActive(false);
+    }
+  }, [index, titles.length]);
+
+  return (
+    <h1
+      className="w-max flex text-4xl sm:text-5xl tracking-tight font-bold"
+      aria-label={`Current title: ${titles[index]}`}
+    >
+      <span className="mr-2">Hey,</span>
+      <span
+        className={`pr-5 bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent font-bold ${
+          animationActive ? 'animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white' : ''
+        }`}
+      >
+        {titles[index]}
+      </span>
+    </h1>
+  );
+};
+
+export default TitleComponent;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,40 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Onest Variable', ...defaultTheme.fontFamily.sans],
+        sans: ["Onest Variable", ...defaultTheme.fontFamily.sans],
       },
-	  backgroundImage: {
-        'custom-linear': 'linear-gradient(to bottom, #0f1216, #182430, #1b374d, #184b6b, #00618b)',
-		
+      backgroundImage: {
+        "custom-linear":
+          "linear-gradient(to bottom, #0f1216, #182430, #1b374d, #184b6b, #00618b)",
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 3s steps(30) infinite alternate, blink .7s infinite",
       },
     },
   },
   plugins: [],
-}
+};
