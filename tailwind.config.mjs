c26ea0bfd85457d56ac5c2206e1a1583d6f41341
Jose Martinez <andresjose.210299@gmail.com>
2024-07-24 22:55:53 -0400

    darkMode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,11 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ["Onest Variable", ...defaultTheme.fontFamily.sans],
-      },
-      backgroundImage: {
-        "custom-linear":
-          "linear-gradient(to bottom, #0f1216, #182430, #1b374d, #184b6b, #00618b)",
       },
       keyframes: {
         typing: {
